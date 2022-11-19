92e59c2f8aab22d2dc8acaf4021e4506676f0d35
kopious <kopious@gmail.com>
2022-11-19 19:31:41 +0000

    update readme to reflect switch to assetfinder. update checkdeps for all tools in the primary workflow

--- checkdeps.sh
+++ checkdeps.sh
@@ -17,7 +17,7 @@ PNAME=`ps -ocomm --no-header $PPID`
 RECON_DIR=~/recon
 
 echo -en "\nChecking dependencies... "
-for name in sublist3r
+for name in assetfinder nuclei fff httprobe
 do
   [[ $(which $name 2>/dev/null) ]] || { echo -en "\n$name needs to be installed. Use 'sudo apt-get install $name'";deps=1; }
 done
